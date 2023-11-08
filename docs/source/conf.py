4ba0308871703a6ae245f95dd70ab7c50fa858f0
Andrea Franceschini <andrea.franceschini03@gmail.com>
2023-11-08 11:58:41 +0100

    numpydoc, autosummary and intersphinx

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,12 +20,30 @@ import sys
 print(os.path.abspath("../../src"))
 sys.path.insert(0, os.path.abspath("../../src"))
 
-extensions = ["sphinx.ext.autodoc", "nbsphinx", "numpydoc"]
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.doctest",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.todo",
+    "numpydoc",
+    "sphinx.ext.ifconfig",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.imgmath",
+    "nbsphinx",
+]
 
 templates_path = ["_templates"]
 exclude_patterns = []
 
+autosummary_generate = True
+
+# -- Options for NumpyDocs  ---------------------------------------------
+numpydoc_xref_param_type = True
+numpydoc_xref_ignore = {"optional", "type_without_description", "BadException"}
 numpydoc_class_members_toctree = False
+numpydoc_validation_checks = {"all", "GL01", "SA04", "RT03"}
+pygments_style = "sphinx"
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -33,3 +51,15 @@ numpydoc_class_members_toctree = False
 
 html_theme = "pydata_sphinx_theme"
 html_static_path = ["_static"]
+
+# -- Intersphinx setup ----------------------------------------------------
+
+# Example configuration for intersphinx: refer to the Python standard library.
+
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/3/", None),
+    "numpy": ("https://numpy.org/devdocs/", None),
+    "sklearn": ("https://scikit-learn.org/stable/", None),
+    "matplotlib": ("https://matplotlib.org/stable/", None),
+    "pandas": ("https://pandas.pydata.org/docs/", None),
+}
