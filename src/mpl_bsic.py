76700a99d05e6d24c2593ef4c59d28390d45350a
Andrea Franceschini <andrea.franceschini03@gmail.com>
2023-11-08 11:59:46 +0100

    mpl_bsic first commit

---
+++ src/mpl_bsic.py
@@ -0,0 +1,43 @@
+"""Lorem ipsum dolor sit amet
+"""
+
+import matplotlib.pyplot as plt
+from matplotlib.figure import Figure
+from matplotlib.axes import Axes
+from cycler import cycler
+
+DFT_TITLE_STYLE = {
+    "fontname": "Gill Sans MT",
+    "color": "black",
+    "fontweight": "bold",
+    "fontstyle": "italic",
+    "fontsize": 12,
+}
+"""TODO"""
+
+DFT_COLOR_CYCLE = cycler(color=["#38329A", "#8EC6FF", "#601E66", "#2F2984", "#0E0B54"])
+"""Default Color Style"""
+DFT_FONT_SIZE = 10
+"""TODO"""
+BSIC_FONT_FAMILY = "Garamond"
+"""TODO"""
+
+
+def apply_BSIC_style(fig: Figure, ax: Axes):
+    """
+    Apply the BSIC Style to the plot. Sets the title style to the correct format.
+
+    Parameters
+    ----------
+    fig : ``matplotlib.figure.Figure``
+        Matplotlib Figure instance.
+    ax : ``matplotlib.axes.Axes``
+        Matplotlib Axes instance.
+    """
+    plt.rcParams["font.sans-serif"] = BSIC_FONT_FAMILY
+    plt.rcParams["font.size"] = DFT_FONT_SIZE
+    plt.rcParams["axes.prop_cycle"] = DFT_COLOR_CYCLE
+
+    title = ax.get_title()
+
+    ax.set_title(title, **DFT_TITLE_STYLE)
