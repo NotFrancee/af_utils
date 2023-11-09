723927fdfcd177b988ca5b7e2ebc9c9909c9e057
Andrea Franceschini <andrea.franceschini03@gmail.com>
2023-11-09 02:35:13 +0100

    use new mpl_bsic module

--- tests/plotter3.test.py
+++ tests/plotter3.test.py
@@ -1,20 +1,19 @@
+from matplotlib.axes import Axes
 import numpy as np
 import sys
 import os
 import matplotlib.pyplot as plt
 
 sys.path.append(os.path.dirname(os.path.realpath(__file__)) + "/../src")
-from bsicplotter import BSICPlotter
-
+from mpl_bsic import apply_BSIC_style
 
 x = np.linspace(0, 5, 100)
 y = np.sin(x)
 
 fig, ax = plt.subplots(1, 1)
+ax: Axes
+apply_BSIC_style(fig, ax, "Sin(x)")
 
 ax.plot(x, y)
-ax.set_title("Sin, cosin")
-plotter = BSICPlotter()
-plotter.apply_BSIC_style(fig, ax)
 
 plt.show()
