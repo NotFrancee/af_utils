bd62c790fbba87b0012e53c58e61543f52630c32
Andrea Franceschini <andrea.franceschini03@gmail.com>
2023-11-09 02:34:09 +0100

    added preprocess_dataframe

--- src/mpl_bsic.py
+++ src/mpl_bsic.py
@@ -91,6 +91,32 @@ This is the examples section. WIP.
 """
 
 
+def preprocess_dataframe(df: pd.DataFrame):
+    """Handle and preprocess the DataFrame before plotting.
+
+    Handle and preprocess the DataFrame before plotting. It sets all the columns to lowercase and sets the index as the dates (converting to datetime).
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        The DataFrame to preprocess.
+
+    See Also
+    --------
+    mpl_bsic.apply_BSIC_style : The function that applies the style to the plot.
+
+    Examples
+    --------
+    This is the examples section. WIP.
+    """
+
+    df.columns = [col.lower() for col in df.columns]
+
+    if "date" in df.columns:
+        df.set_index("date", inplace=True, drop=True)
+        df.index = pd.to_datetime(df.index)
+
+
     r"""Apply the BSIC Style to an existing matplotlib plot.
 
     Apply the BSIC Style to the plot. First, it sets the font family and size for the overall plot and the color cycle to use.
