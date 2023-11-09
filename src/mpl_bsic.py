a52248b75d9e2e77e91567ca42cc0f003487d8bf
Andrea Franceschini <andrea.franceschini03@gmail.com>
2023-11-09 02:33:36 +0100

    added numpydoc docstrings

--- src/mpl_bsic.py
+++ src/mpl_bsic.py
@@ -1,4 +1,4 @@
-"""Lorem ipsum dolor sit amet
+"""``mpl_bsic`` helps you style matplotlib plots in BSIC style.
 """
 
 import matplotlib.pyplot as plt
@@ -13,32 +13,141 @@ DEFAULT_TITLE_STYLE = {
     "fontstyle": "italic",
     "fontsize": 12,
 }
-"""TODO"""
+"""Default Title Style. Used in ``apply_BSIC_style``.
+
+Details: 
+
+* ``fontname``: ``Gill Sans MT``
+* ``color``: ``black``
+* ``fontweight``: ``bold``
+* ``fontstyle``: ``italic``
+* ``fontsize``: ``12``
+
+See Also 
+--------
+mpl_bsic.apply_BSIC_style : The function that applies the style to the plot.
+mpl_bsic.DEFAULT_COLOR_CYCLE : The default color cycler that gets applied to the plot.
+mpl_bsic.DEFAULT_FONT_SIZE : The default font size that gets applied to the plot.
+
+Examples
+--------
+This is the examples section. WIP.
+"""
 
 DEFAULT_COLOR_CYCLE = cycler(
     color=["#38329A", "#8EC6FF", "#601E66", "#2F2984", "#0E0B54"]
 )
+"""Default Color Style.
+
+Cycle: 
+
+* ``#38329A`` 
+* ``#8EC6FF`` 
+* ``#601E66`` 
+* ``#2F2984`` 
+* ``#0E0B54``
+
+See Also 
+--------
+mpl_bsic.apply_BSIC_style : The function that applies the style to the plot.
+mpl_bsic.DEFAULT_TITLE_STYLE : The default title style that gets applied to the plot.
+mpl_bsic.DEFAULT_FONT_SIZE : The default font size that gets applied to the plot.
+
+Examples
+--------
+This is the examples section. WIP.
+"""
 
 DEFAULT_FONT_SIZE = 10
+"""Default Font Size for the plot (text, labels, ticks).
+
+The default font size used for the plots is 10.
+
+See Also 
+--------
+mpl_bsic.apply_BSIC_style : The function that applies the style to the plot.
+mpl_bsic.DEFAULT_TITLE_STYLE : The default title style that gets applied to the plot.
+mpl_bsic.DEFAULT_COLOR_CYCLE : The default color cycler that gets applied to the plot.
+
+Examples
+--------
+This is the examples section. WIP.
+"""
+
 BSIC_FONT_FAMILY = "Garamond"
-"""TODO"""
+"""Default Font Family for the plot (text, labels, ticks).
+
+The default font family used for the plots is ``Garamond``.
+
+See Also 
+--------
+mpl_bsic.apply_BSIC_style : The function that applies the style to the plot.
+mpl_bsic.DEFAULT_TITLE_STYLE : The default title style that gets applied to the plot.
+mpl_bsic.DEFAULT_FONT_SIZE : The default font size that gets applied to the plot.
+
+Examples
+--------
+This is the examples section. WIP.
+"""
 
 
-def apply_BSIC_style(fig: Figure, ax: Axes):
-    """
-    Apply the BSIC Style to the plot. Sets the title style to the correct format.
+    r"""Apply the BSIC Style to an existing matplotlib plot.
+
+    Apply the BSIC Style to the plot. First, it sets the font family and size for the overall plot and the color cycle to use.
+    Then, if the plot has a title, then it applies the default title style.
+
+    Should be called *before* plotting, to make sure the right color cycle gets applied.
+
+    Warning: if you want to specify and set a title to the plot, you can either set it before or give it to the function.
+    Otherwise, the correct style won't be applied. This is forced by matplotlib and must be done to make sure the fuction works.
 
     Parameters
     ----------
-    fig : ``matplotlib.figure.Figure``
+    fig : matplotlib.figure.Figure
         Matplotlib Figure instance.
-    ax : ``matplotlib.axes.Axes``
+    ax : matplotlib.axes.Axes
         Matplotlib Axes instance.
+    title : str | None
+        Title of the plot. If None, it will try to get the title from the Axes instance.
+
+    See Also
+    --------
+    mpl_bsic.DEFAULT_TITLE_STYLE : The default title style that gets applied to the plot.
+    mpl_bsic.DEFAULT_COLOR_CYCLE : The default color cycler that gets applied to the plot.
+    mpl_bsic.DEFAULT_FONT_SIZE : The default font size that gets applied to the plot.
+
+    Examples
+    --------
+    .. plot::
+
+        from mpl_bsic import apply_BSIC_style
+
+        x = np.linspace(0, 5, 100)
+        y = np.sin(x)
+
+        fig, ax = plt.subplots(1, 1)
+        apply_BSIC_style(fig, ax, 'Sin(x)') # apply right after creating the Figure and Axes instances
+
+        ax.plot(x,y)
+
+    .. plot::
+
+        from mpl_bsic import apply_BSIC_style
+
+        x = np.linspace(0, 5, 100)
+        y = np.cos(x)
+
+        fig, ax = plt.subplots(1, 1)
+        # ax.set_title('Cos(x)') # set the title before applying the style
+        apply_BSIC_style(fig, ax) # the function will re-set the title with the correct style
+
+        ax.plot(x,y)
     """
     plt.rcParams["font.sans-serif"] = BSIC_FONT_FAMILY
-    plt.rcParams["font.size"] = DFT_FONT_SIZE
-    plt.rcParams["axes.prop_cycle"] = DFT_COLOR_CYCLE
+    plt.rcParams["font.size"] = DEFAULT_FONT_SIZE
+    plt.rcParams["axes.prop_cycle"] = DEFAULT_COLOR_CYCLE
+    ax.set_prop_cycle(DEFAULT_COLOR_CYCLE)
 
     title = ax.get_title()
 
-    ax.set_title(title, **DFT_TITLE_STYLE)
+    ax.set_title(title, **DEFAULT_TITLE_STYLE)
