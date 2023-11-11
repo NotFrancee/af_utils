23916a1518371336cac2014b3596b49786474d41
Andrea Franceschini <andrea.franceschini2@studbocconi.it>
2023-11-11 12:32:58 +0100

    changed BSIC to bsic

--- tests/plotter3.test.py
+++ tests/plotter3.test.py
@@ -5,14 +5,15 @@ import os
 import matplotlib.pyplot as plt
 
 sys.path.append(os.path.dirname(os.path.realpath(__file__)) + "/../src")
-from mpl_bsic import apply_BSIC_style
+from mpl_bsic import apply_bsic_style
+
 
 x = np.linspace(0, 5, 100)
 y = np.sin(x)
 
 fig, ax = plt.subplots(1, 1)
 ax: Axes
-apply_BSIC_style(fig, ax, "Sin(x)")
+apply_bsic_style(fig, ax, "Sin(x)")
 
 ax.plot(x, y)
 
